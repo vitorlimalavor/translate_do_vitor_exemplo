ee999f12b97ee3c082f61bd55a34a12d1633b8f0
Vitor Lima de Lavor <79912338+githubodc@users.noreply.github.com>
2025-03-18 17:38:33 -0300

    add voice for example

--- translator.py
+++ translator.py
@@ -2,12 +2,10 @@ import streamlit as st
 import pprint
 import asyncio
 from googletrans import Translator
+from gtts import gTTS
+from io import BytesIO
 
-st.title("Super Tradutor do Vitinho")
-
-
-
-
+st.title("Super Tradutor do Vitor")
 async def translate_text(phrase,src,dest):
     async with Translator() as translator:
         
@@ -15,15 +13,23 @@ async def translate_text(phrase,src,dest):
         return result  # <Translated src=la dest=en text=The truth is my light pronunciation=The truth is my light>
 
 
-phrase = st.text_input("Type this phrase")
+def voice(phrase):
+    mp3_fp = BytesIO()
+    tts = gTTS(phrase, lang='en')
+    tts.write_to_fp(mp3_fp)
+    mp3_fp.seek(0)
+    return mp3_fp
+
+
+phrase = st.text_input("Type your phrase")
 if st.button("Traduzir"):
     with st.spinner("Gerando conteúdo..."):
-            content = asyncio.run(translate_text(phrase,'en','pt'))          
-            st.success("Successful Translation ")
-            st.text_area("Translation: ",content, height=200)
-    
-
-
+            content = asyncio.run(translate_text(phrase,'pt','en'))          
+            st.success("Translation: ")
+            st.text_area("Translation: ",content.text, height=70)
+            audio_test = voice(content.text)
+            st.success("Voice")
+            st.audio(audio_test, format="audio/mpeg")
 
 
 
